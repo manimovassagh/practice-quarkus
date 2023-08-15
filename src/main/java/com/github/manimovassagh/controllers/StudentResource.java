ef6ef1a55f79654116eb04cb02a725e4eeffe9c8
Mani Movassagh <mani.mgh@gmail.com>
2023-08-15 17:32:57 +0200

    add list of student

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -1,10 +1,27 @@
 package com.github.manimovassagh.controllers;
 
 
+import com.github.manimovassagh.entities.Student;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
 public class StudentResource {
+
+
+
+    @GetMapping
+    public List<Student> getStudents(){
+        List<Student> students=new ArrayList<>();
+        Student student1=Student.builder().withId(2L).withName("Mani").withCourse("React").build();
+        Student student=Student.builder().withId(3L).withName("Sahar").withCourse("Ui UX").build();
+        students.add(student1);
+        students.add(student);
+        return students;
+    }
 }
