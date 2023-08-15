e9d5e14de3b2535982f54df377d62501dbae22a9
Mani Movassagh <mani.mgh@gmail.com>
2023-08-15 17:24:22 +0200

    add builder pattern

--- src/main/java/com/github/manimovassagh/GreetingController.java
+++ src/main/java/com/github/manimovassagh/GreetingController.java
@@ -1,5 +1,6 @@
 package com.github.manimovassagh;
 
+import com.github.manimovassagh.entities.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,12 @@ public class GreetingController {
     @GetMapping
     public String hello() {
         return "Hello Spring";
+    }
+
+
+
+    @GetMapping(path = "/student")
+    public Student getStudent(){
+        return Student.builder().withId(1L).withName("Sahar").withCourse("Java").build();
     }
 }
---
+++ src/main/java/com/github/manimovassagh/entities/Student.java
@@ -0,0 +1,76 @@
+package com.github.manimovassagh.entities;
+
+import java.util.Objects;
+
+public class Student {
+
+    private Long id;
+    private String name;
+    private String course;
+
+    private Student() {
+    }
+
+    private Student(Long id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    public static class StudentBuilder {
+        private Long id;
+        private String name;
+        private String course;
+
+        public StudentBuilder() {
+        }
+
+        public StudentBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public StudentBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public StudentBuilder withCourse(String course) {
+            this.course = course;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(id, name, course);
+        }
+    }
+
+    public static StudentBuilder builder() {
+        return new StudentBuilder();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(course, student.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, course);
+    }
+}
