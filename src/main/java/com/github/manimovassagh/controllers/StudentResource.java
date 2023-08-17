058eb05752f18d2ed7d1ef265f0c266dd0c24fb4
Mani Movassagh <mani.mgh@gmail.com>
2023-08-17 23:10:06 +0200

    add docs

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -25,7 +25,7 @@ public class StudentResource {
         Student.persist(students);
         return students;
     }
-//check map
+//check map struct
     @GetMapping(path = "/db")
     public List<Student> getFromDb(){
         return Student.listAll();
