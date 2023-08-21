f06e4efa2358580c39f00a8382c7d0be26212eaa
Mani Movassagh <mani.mgh@gmail.com>
2023-08-22 00:05:34 +0200

    test some

--- src/main/java/com/github/manimovassagh/GreetingController.java
+++ src/main/java/com/github/manimovassagh/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
     }
 
 
-//test some files to
+//test some files t
     @GetMapping(path = "/student")
     public Student getStudent(){
         return Student.builder().withId(1L).withName("Sahar").withCourse("Java").build();
