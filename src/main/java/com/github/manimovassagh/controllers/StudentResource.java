26f74f2949d14ff71ee5c75e328e7ca8716ae445
Mani Movassagh <mani.mgh@gmail.com>
2023-08-24 21:49:04 +0200

    test some

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -25,7 +25,7 @@ public class StudentResource {
         Student.persist(students);
         return students;
     }
-//checki
+//check
     @GetMapping(path = "/db")
     public List<Student> getFromDb(){
         return Student.listAll();
