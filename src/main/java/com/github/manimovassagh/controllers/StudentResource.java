2517d0356eeb08f95d9dec2c6049e5536862b082
Mani Movassagh <mani.mgh@gmail.com>
2023-08-28 01:47:32 +0200

    test some

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -26,7 +26,7 @@ public class StudentResource {
         return students;
     }
 
-    //more controllers some
+    //more controllers som
     @GetMapping(path = "/check")
     public List<Student> getStudentCheck(){
         List<Student> students=new ArrayList<>();
