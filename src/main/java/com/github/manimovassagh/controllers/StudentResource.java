fbd240b2817caf391df8a6b682f30171943c538b
Mani Movassagh <mani.mgh@gmail.com>
2023-08-29 00:32:26 +0200

    test some

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class StudentResource {
 
 
-//mantttt
+//manttt
     @GetMapping
     public List<Student> getStudents(){
         List<Student> students=new ArrayList<>();
