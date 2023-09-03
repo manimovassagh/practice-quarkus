1b88805d9a60189d923c830a904b0ae34f8e4da4
Mani Movassagh <mani.mgh@gmail.com>
2023-09-03 02:00:46 +0200

    test some

--- src/main/java/com/github/manimovassagh/GreetingController.java
+++ src/main/java/com/github/manimovassagh/GreetingController.java
@@ -36,6 +36,9 @@ public class GreetingController {
        StringAccept acceptInteger=new StringAccept(12);
        list.add(acceptInteger);
        list.add(accept);
+       list.add(accept);
+       list.add(accept);
+       list.add(accept);
         return  list;
     }
 
