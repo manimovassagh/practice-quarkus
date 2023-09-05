633c006bcbc9384719b6ea161210d423189c32c2
Mani Movassagh <mani.mgh@gmail.com>
2023-09-05 18:21:56 +0200

    test some

--- src/main/java/com/github/manimovassagh/GreetingResource.java
+++ src/main/java/com/github/manimovassagh/GreetingResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-//check that
+//check that o
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
