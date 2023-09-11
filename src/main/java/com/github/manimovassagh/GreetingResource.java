32bb18fbd93799358819e62c48596caf1383ab1e
Mani Movassagh <mani.mgh@gmail.com>
2023-09-11 23:06:07 +0200

    new commit

--- src/main/java/com/github/manimovassagh/GreetingResource.java
+++ src/main/java/com/github/manimovassagh/GreetingResource.java
@@ -14,7 +14,7 @@ public class GreetingResource {
         return "Hello from RESTEasy Reactive";
     }
 
-//tettnnmmdd
+//tettnnmmd
     @GET()
     @Path("/new")
     @Produces(MediaType.TEXT_PLAIN)
