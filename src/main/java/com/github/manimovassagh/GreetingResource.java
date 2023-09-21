bfbb14a3d864a6a86266777d3a02536d45e8abcc
Mani Movassagh <mani.mgh@gmail.com>
2023-09-21 21:32:15 +0200

    new commit

--- src/main/java/com/github/manimovassagh/GreetingResource.java
+++ src/main/java/com/github/manimovassagh/GreetingResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-    //ttllm
+    //ttll
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
