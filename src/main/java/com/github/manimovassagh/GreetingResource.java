0a3e6ab9faac98d7680f0b2e472978d0462945d2
Mani Movassagh <mani.mgh@gmail.com>
2023-09-22 17:17:13 +0200

    docs

--- src/main/java/com/github/manimovassagh/GreetingResource.java
+++ src/main/java/com/github/manimovassagh/GreetingResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-    //add controller
+    //add controll
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
