4b0301c01bbbca515b579bb1d8ea7355cd5cbf11
Mani Movassagh <mani.mgh@gmail.com>
2023-10-23 01:33:16 +0200

    docs

--- src/main/java/com/github/manimovassagh/controllers/StudentResource.java
+++ src/main/java/com/github/manimovassagh/controllers/StudentResource.java
@@ -27,7 +27,7 @@ public class StudentResource {
         students.add(student);
         Student.persist(students);
         return students;
-    }    //more controller in th
+    }    //more controller in t
 
 
     @GetMapping(path = "/check")
